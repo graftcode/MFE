70760fda46bc75cbb3771827940b6e9d4238179c
graftcode <graftcode@outlook.com>
2021-12-09 22:39:02 +0000

    container - webpack production config

--- container/webpack.common.js
+++ container/webpack.common.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   module: {
@@ -17,4 +16,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
--- container/webpack.prod.js
+++ container/webpack.prod.js
@@ -1,7 +1,24 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
 
+const { dependencies } = require("./package.json");
 
-module.exports = {
-    
-}
+const domain = process.env.PRODUCTION_DOMAIN;
+
+module.exports = merge(commonConfig, {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container", //for host this never gets used but convention to use
+      remotes: {
+        //marketing in string matches up with name defined in wp in marketing...The key marketing whenever that is imported we are going to look at the url and look inside there for it
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: dependencies,
+    }),
+  ],
+});
