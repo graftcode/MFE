33a9ad3e7822875202f5fa12308c5a02a33222f8
graftcode <graftcode@outlook.com>
2021-12-09 22:46:25 +0000

    marketing - webpack prod config

--- marketing/webpack.prod.js
+++ marketing/webpack.prod.js
@@ -1,7 +1,28 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
+const { dependencies } = require("./package.json");
 
-module.exports = {
-    
-}
+const domain = process.env.PRODUCTION_DOMAIN;
+
+module.exports = merge(commonConfig, {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing", //going to be some global variable
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+});
