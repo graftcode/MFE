8a0a5a9ef5187752fb3fa3633dc6a0b7c9b8e2db
graftcode <graftcode@outlook.com>
2021-12-09 20:44:03 +0000

    set up container application

---
+++ container/webpack.dev.js
@@ -0,0 +1,19 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+
+module.exports = merge(commonConfig, {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+});
