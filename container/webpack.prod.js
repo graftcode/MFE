eeac1e674055c58a277dacfc351e063734897463
graftcode <graftcode@outlook.com>
2021-12-11 17:58:47 +0000

    marketing - add marketing deploymnet

--- container/webpack.prod.js
+++ container/webpack.prod.js
@@ -17,7 +17,7 @@ module.exports = merge(commonConfig, {
       name: "container", //for host this never gets used but convention to use
       remotes: {
         //marketing in string matches up with name defined in wp in marketing...The key marketing whenever that is imported we are going to look at the url and look inside there for it
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: dependencies,
     }),
