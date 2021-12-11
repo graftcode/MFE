f09f326cca2fd4ce4fae5616adaa7fda8ccc8d04
graftcode <graftcode@outlook.com>
2021-12-11 20:18:09 +0000

    fix wp

--- container/webpack.prod.js
+++ container/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(commonConfig, {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/container/latest/"
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
