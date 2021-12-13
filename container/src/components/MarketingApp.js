1e460e96995af9e54fd5447f31a6d50b5e7a4d2f
graftcode <graftcode@outlook.com>
2021-12-13 22:17:31 +0000

    container to child communication

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -5,15 +5,18 @@ import { useHistory, usseHistory } from "react-router-dom";
 export default () => {
   const ref = useRef(null);
   const history = useHistory();
+
   useEffect(() => {
-    mount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
 
         if (pathname !== nextPathname) history.push(nextPathname);
       },
     });
-  });
+
+    history.listen(onParentNavigate);//will make an arg available inside onParentNavigate called location
+  }, []);
 
   return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -9,6 +9,14 @@ const mount = (el, { onNavigate }) => {
   if (onNavigate) history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) history.push(nextPathname);
+    },
+  };
 };
 
 //if in development and in isolation, call mount immediately
