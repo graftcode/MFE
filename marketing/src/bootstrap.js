24d3b2bd01041cddb20487c43f597deb944c1be4
graftcode <graftcode@outlook.com>
2021-12-13 22:00:26 +0000

    Marketing - run memory history in isolation (fix)

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -6,16 +6,15 @@ import App from "./App";
 // MOunt function to start the app
 const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
+  if (onNavigate) history.listen(onNavigate);
 
-  // history.listen(onNavigate);
-  history.listen(onNavigate);
   ReactDOM.render(<App history={history} />, el);
 };
 
 //if in development and in isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
-  if (devRoot) mount(devRoot);
+  if (devRoot) mount(devRoot, {});
 }
 
 // We are running through container and should export mount()
