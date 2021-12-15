0ac166fbfc289b02d11127ebcd23fcde9bb3130d
graftcode <graftcode@outlook.com>
2021-12-15 22:16:02 +0000

    container - add auth subapp

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
-import Header from "./components/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import AuthApp from "./components/AuthApp";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -15,10 +17,11 @@ const App = () => {
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
+        <Header />
+        <Switch>
+          <Route path="/auth" component={AuthApp} />
+          <Route path="/" component={MarketingApp} />
+        </Switch>
       </BrowserRouter>
     </StylesProvider>
   );
